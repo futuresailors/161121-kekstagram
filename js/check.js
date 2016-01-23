492473859abc896d9bad9f71e7f2779c9dfddb86
Artem Gladkov <ok.gladkov@gmail.com>
2016-01-23 15:44:13 +0300

    Module1-task2
    Добавил файл check.js

---
+++ js/check.js
@@ -0,0 +1,45 @@
+
+
+function getMessage (a, b) {
+  if (typeof a == 'boolean' && a == true )
+  {
+    return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
+  }
+
+  else {
+    if (a == false) {
+      return ('Переданное GIF-изображение не анимировано');
+    }
+
+    else {
+      if (typeof a == 'number') {
+        return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b*4) + ' аттрибутов');
+      }
+
+      else {
+        if (typeof b != 'object') {
+          var sum=0;
+
+          for(var i=0; i<a.length; i++) {
+            sum = sum + a[i];
+          }
+          return ('Количество красных точек во всех строчках изображения: ' + sum);
+        }
+
+        else {
+          if (typeof a == 'object' && typeof b == 'object') {
+            var square = 0;
+
+            for(var i=0; i<b.length; i++) {
+              square = square + (a[i] * b[i])
+            }
+
+            return ('Общая площадь артефактов сжатия: ' + square + ' пикселей');
+          }
+        }
+      }
+    }
+  }
+}
+
+getMessage();
