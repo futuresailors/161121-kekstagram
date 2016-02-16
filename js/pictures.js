5d7d81987e9b0cf8138a88a9ccc7b7cc2997503b
Artem Gladkov <ok.gladkov@gmail.com>
2016-02-16 16:16:43 +0300

    Module5 Task1
    Сортировка, первый подход.

--- js/pictures.js
+++ js/pictures.js
@@ -1,4 +1,3 @@
-/* global pictures: true */
 'use strict';
 (function() {
 
@@ -8,13 +7,73 @@
 
   var container = document.querySelector('.pictures');
 
-  pictures.forEach(function(picture, idx, array) {
-    var element = getElementFromTemplate(picture);
-    container.appendChild(element);
-    if (idx === array.length - 1) {
-      filterMenu.classList.remove('hidden');
+  var filters = document.querySelectorAll('.filters-radio');
+
+  var pictures = [];
+
+  var activeFilter = 'filter-popular';
+
+  for (var i = 0; i < filters.length; i++) {
+    filters[i].onclick = function(evt) {
+      var clickedElementID = evt.target.id;
+      setActiveFilter(clickedElementID);
+    };
+  }
+
+  getPictures();
+
+  function renderPictures(pictures) {
+    container.innerHTML = '';
+
+    pictures.forEach(function(picture, idx, array) {
+      var element = getElementFromTemplate(picture);
+      container.appendChild(element);
+      if (idx === array.length - 1) {
+        filterMenu.classList.remove('hidden');
+      }
+      console.log(pictures);
+    });
+  }
+
+  function setActiveFilter(id) {
+    if (activeFilter === id) {
+      return;
     }
-  });
+
+    document.querySelector('#' + activeFilter).checked = false;
+    document.querySelector('#' + id).checked = true;
+
+    var filteredPictures = pictures.slice(0);
+    //console.log(filteredPictures);
+
+    switch (id) {
+      case 'filter-popular':
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          return a.date - b.date; });
+        break;
+      case 'filter-new':
+        //filteredPictures = filteredPictures.sort(function(a, b) {return b - a;});
+        filteredPictures = filteredPictures;
+    }
+
+    renderPictures(filteredPictures);
+  }
+
+  function getPictures() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '//o0.github.io/assets/json/pictures.json', true);
+    //xhr.open('GET', 'api.openweathermap.org/data/2.5/weather?q=London', true);
+    xhr.onload = function(evt) {
+      var rawData = evt.srcElement.response;
+      var loadedPictures = JSON.parse(rawData);
+      renderPictures(loadedPictures);
+      //console.log(evt);
+      //console.log(loadedPictures);
+    };
+    xhr.send();
+    xhr.timeout = 10000;
+  }
+
 
   function getElementFromTemplate(data) {
     var template = document.querySelector('#picture-template');
